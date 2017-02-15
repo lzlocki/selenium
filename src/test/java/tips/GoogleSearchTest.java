2bc53663e773a8ea3c62b8b39d7a2ab3eba59bed
Testerzy <worksoft@onet.eu>
2017-02-15 17:04:47 +0100

    changes

--- src/test/java/tips/GoogleSearchTest.java
+++ src/test/java/tips/GoogleSearchTest.java
@@ -1,12 +1,14 @@
 package tips;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Created by andrew on 8/22/15.
@@ -16,8 +18,9 @@ public class GoogleSearchTest {
 
     WebDriver driver;
 
-    @Before
+    @BeforeTest
     public void setUp() throws Exception {
+    	System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/vendor/geckodriver.exe");
         driver = new FirefoxDriver();
     }
 
@@ -25,11 +28,11 @@ public class GoogleSearchTest {
     public void workWithBasicAuthTest() {
         GoogleSearch google = new GoogleSearch(driver);
         google.searchFor("elemental selenium tips");
-        boolean result = google.searchResultPresent("Recieve a Free, Weekly tip");
-        assert (result == true);
+        boolean result = google.searchResultPresent("Receive a Free, Weekly Tip");
+        Assert.assertTrue(result);
     }
 
-    @After
+    @AfterTest
     public void tearDown() throws Exception {
         driver.quit();
     }
@@ -40,14 +43,14 @@ class GoogleSearch {
     private final String BASE_URL = "http://www.google.com";
     WebDriver driver;
 
-    By searchBox = By.id("gbqfq");
-    By searchBoxSubmit = By.id("gbqfb");
-    By topSearchResult = By.cssSelector("#rso .g");
+    By searchBox = By.name("q");
+    By searchBoxSubmit = By.name("q");
+    By topSearchResult = By.cssSelector("#rso .g:nth-child(1)");
 
-    public GoogleSearch(WebDriver _driver) {
-        this.driver = _driver;
+    public GoogleSearch(WebDriver driver) {
+        this.driver = driver;
         visit();
-        assert (verifyPage() == true);
+        Assert.assertTrue(verifyPage());
     }
 
     public void visit() {
@@ -70,6 +73,7 @@ class GoogleSearch {
     }
 
     public boolean verifyPage() {
-        return driver.getCurrentUrl().contains("Google");
+    	System.out.println(driver.getCurrentUrl());
+        return driver.getCurrentUrl().contains("google");
     }
 }
